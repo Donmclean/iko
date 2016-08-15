1f01bc97233f7290b670482f70801986a90f519b
Don <donmcleanx@gmail.com>
2016-08-15 17:39:10 -0400

    update image task

--- gulp/tasks/_1_images.js
+++ gulp/tasks/_1_images.js
@@ -7,11 +7,11 @@ module.exports = (gulp, $, config, funcs) => {
 
         return gulp.src(config.media.images.src)
             .pipe($.plumber({errorHandler: funcs.gulpGlobalErrorHandler}))
-            .pipe($.debug({title: 'copying images:'}))
-            // .pipe($.imagemin({
-            //     progressive: true,
-            //     use: [config.vars.pngquant()]
-            // }))
+            .pipe($.if(!!funcs.minifyImages, $.debug({title: 'copying & minifying images:'}), $.debug({title: 'copying images:'})))
+            .pipe($.if(!!funcs.minifyImages, $.imagemin({
+                progressive: true,
+                use: [config.vars.pngquant()]
+            })))
             .pipe($.if(!!funcs.isDev, gulp.dest(config.media.images.tempDir)))
             .pipe($.if(!funcs.isDev, gulp.dest(config.media.images.destDir)));
 
